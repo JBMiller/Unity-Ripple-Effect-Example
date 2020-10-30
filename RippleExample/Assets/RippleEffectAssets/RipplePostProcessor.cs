10b8da130aa87e8edbb4bb8955c2a726b0f5bbe2
Jonathan B Miller <jonathanbmi10@gmail.com>
2020-10-30 11:37:48 -0400

    Init Commit

---
+++ RippleExample/Assets/RippleEffectAssets/RipplePostProcessor.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RipplePostProcessor : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton reference to access RipplePostProcessor anywhere.
+    /// You can change this to any other kind of reference that works for your project.
+    /// </summary>
+    public static RipplePostProcessor ins;
+
+
+    private const float EXPECTED_DELTATIME_AT_60FPS = 1f / 60f;
+    public const float LOWEST_AMOUNT_VALUE = 0.0001f;
+    public Material RippleMaterial;
+    public float MaxAmount = 50f;
+
+    [Range(0, 1)]
+    public float Friction = .9f;
+
+    public float CurrentAmount = 0f;
+    private bool _update = false;
+
+    public bool DEBUG_ToggleRipple;
+
+    private void Awake()
+    {
+        if (ins != null)
+        {
+            Destroy(this);
+            return;
+        }
+        ins = this;
+    }
+    private void Start()
+    {
+        CurrentAmount = RippleMaterial.GetFloat("_Amount");
+        _update = CurrentAmount > LOWEST_AMOUNT_VALUE;
+    }
+    public void Ripple()
+    {
+        CurrentAmount = MaxAmount;
+        Vector2 pos = new Vector2(Screen.width, Screen.height) / 2f;
+        RippleMaterial.SetFloat("_CenterX", pos.x);
+        RippleMaterial.SetFloat("_CenterY", pos.y);
+        _update = true;
+    }
+    void Update()
+    {
+        if (DEBUG_ToggleRipple)
+        {
+            DEBUG_ToggleRipple = false;
+            Ripple();
+        }
+        if (_update)
+        {
+            RippleMaterial.SetFloat("_Amount", CurrentAmount);
+            float amountToReduce = CurrentAmount * (1 - Friction);
+            CurrentAmount -= amountToReduce * (Time.deltaTime / EXPECTED_DELTATIME_AT_60FPS);
+            if (CurrentAmount < LOWEST_AMOUNT_VALUE)
+            {
+                _update = false;
+                CurrentAmount = 0;
+                RippleMaterial.SetFloat("_Amount", CurrentAmount);
+            }
+        }
+    }
+    /// <summary>
+    /// Only works if you dont have Universal/HD Render Pipline installed.
+    /// Delete or comment this out if you dont have URP or HDRP installed.
+    /// </summary>
+    //void OnRenderImage(RenderTexture src, RenderTexture dst)
+    //{
+    //    Graphics.Blit(src, dst, RippleMaterial);
+    //}
+
+    private void OnApplicationQuit()
+    {
+        RippleMaterial.SetFloat("_Amount", 0);
+        RippleMaterial.SetFloat("_CenterX", 0);
+        RippleMaterial.SetFloat("_CenterY", 0);
+    }
+}
---
+++ RippleExample/Assets/RippleEffectAssets/URP_RippleEffectAssets/RippleEffect_RenderPassFeature.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class RippleEffect_RenderPassFeature : ScriptableRendererFeature
+{
+    class RippleEffect_RenderPass : ScriptableRenderPass
+    {
+        static readonly int TempTargetId = Shader.PropertyToID("_Temp_RippleBlit"); // You can name this anything you want
+
+        public RenderTargetIdentifier source;
+        public RenderTargetHandle destination;
+        public int blitShaderPassIndex;
+        public FilterMode filterMode;
+
+        private RenderTargetHandle m_TemporaryColorTexture;
+
+        public RippleEffect_RenderPass(RenderPassEvent renderPassEvent, FilterMode filterMode, int blitShaderPassIndex)
+        {
+            this.renderPassEvent = renderPassEvent;
+            this.filterMode = filterMode;
+            this.blitShaderPassIndex = blitShaderPassIndex;
+            m_TemporaryColorTexture.Init("_TemporaryColorTexture"); // You can name this anything you want
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get();
+
+            if (Application.isPlaying && RipplePostProcessor.ins.CurrentAmount > RipplePostProcessor.LOWEST_AMOUNT_VALUE)
+            {
+                RenderTextureDescriptor opaqueDesc = renderingData.cameraData.cameraTargetDescriptor;
+                opaqueDesc.depthBufferBits = 0;
+
+                // Can't read and write to same color target, use a TemporaryRT
+                if (destination == RenderTargetHandle.CameraTarget)
+                {
+                    cmd.GetTemporaryRT(m_TemporaryColorTexture.id, opaqueDesc, filterMode);
+                    Blit(cmd, source, m_TemporaryColorTexture.Identifier(), RipplePostProcessor.ins.RippleMaterial, blitShaderPassIndex);
+                    Blit(cmd, m_TemporaryColorTexture.Identifier(), source);
+                }
+
+            }
+            // execution
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+
+    RippleEffect_RenderPass m_ScriptablePass;
+
+    /// <summary>Configures where the render pass should be injected.</summary>
+    public FilterMode filterMode = FilterMode.Bilinear;
+    public RenderPassEvent TheRenderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+    public int blitShaderPassIndex = -1;
+    public override void Create()
+    {
+        m_ScriptablePass = new RippleEffect_RenderPass(TheRenderPassEvent, filterMode, blitShaderPassIndex);
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        m_ScriptablePass.source = renderer.cameraColorTarget;
+        m_ScriptablePass.destination = RenderTargetHandle.CameraTarget;
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+}
